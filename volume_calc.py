d470aa8d68cf1f5d4fd651ab95178279d20737a3
Reiya777 <163234234+Reiya777@users.noreply.github.com>
2024-03-14 00:20:58 +0900

    Create volume_calc.py
    중앙대학교 오픈소스SW와 파이썬 프로그래밍 02분반 1주차 과제
    : 사용자에게 가로, 세로, 높이를 입력받아 박스의 부피를 계산하는 프로그램

--- volume_calc.py
+++ volume_calc.py
@@ -1,4 +1,6 @@
 Width = float(input("가로:"))
 Depth = float(input("세로:"))
 Height = float(input("높이:"))
-print("박스의 부피는", Width * Depth * Height, "입니다.")
+Volume = Width * Depth * Height
+print("박스의 부피는")
+print(Volume, "입니다.")
